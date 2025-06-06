cdcb256c1207d9c95d63b8a65547099be1deb91a
Maciej Brencz <maciej.brencz@gmail.com>
2025-06-06 10:44:55 +0100

    Add the `Part` dataclass that can take KiCad `Symbol` data (#4)
    * Add the Part dataclass that can take KiCad Symbol data
    
    * Handle extended symbols
    
    * Part: store the "Footprint" property
    
    * Part: type can also be "no_connect" / name "NC"

---
+++ kicad_parser/part.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+from kicad_sym import KicadSymbol
+
+
+@dataclass
+class Pin:
+    """
+    Keep a single pin definition, a subset of kicad_sym.Pin class
+    """
+    name: str   # e.g. DO, Q3, GND, CLK, VCC
+    type: str  # e.g. tri_state, input, power_in, output, no_connect
+    alt_funcs: Optional[list[str]]  # alternative functions, useful for microprocessors
+
+    def __repr__(self) -> str:
+        return f'<Pin {self.name} [{self.type}]>'
+
+    def as_dict(self) -> dict:
+        return {
+            'name': self.name,
+            'type': self.type,
+        }
+
+
+@dataclass
+class Part:
+    """
+    Keeps the device pinout, a subset of kicad_sym.KicadSymbol class
+    """
+    name: str  # e.g. 74469
+    pinout:  Dict[str, Pin]  # "1": {name: "DO": type: "input"}
+    footprint: Optional[str]
+    datasheet: Optional[str]
+    description: Optional[str]
+
+    def __str__(self) -> str:
+        return f'<Part {self.name} ({self.description})>'
+
+    def __repr__(self) -> str:
+        pinout = '\n'.join([
+            f'\t{no} {repr(pin)}'
+          for no, pin in self.pinout.items()
+        ])
+
+        return f'<Part {self.name}\n\tDesc: {self.description}\n\tPDF: {self.datasheet}\n{pinout}\n>'
+
+    def as_dict(self) -> dict:
+        return {
+            'name': self.name,
+            'description': self.description,
+            'datasheet': self.datasheet,
+            'footprint': self.footprint,
+            'pinout': {no: pin.as_dict() for no, pin in self.pinout.items()},
+        }
+
+    @classmethod
+    def from_kicad_symbol(cls, symbol: KicadSymbol):
+        # Some devices extend other symbols
+        # get_parent_symbol
+        # 	(symbol "7400"
+        # 		(extends "74LS00")
+        parent = symbol.get_parent_symbol()
+
+        pinout = {
+            pin.number: Pin(
+                name=pin.name,
+                type=pin.etype,
+                alt_funcs=[],  # TODO
+            )
+            # pins should be sorted by their numbers
+            for pin in sorted(
+                symbol.pins if not parent else parent.pins,
+                key=lambda p: int(p.number) if str(p.number).isnumeric() else p.number
+            )
+        }
+
+        assert len(pinout.keys()) > 0, f'Pinout of {symbol.name} is empty'
+
+        datasheet = symbol.get_property('Datasheet')
+        description = symbol.get_property('Description')
+        footprint = symbol.get_property('Footprint')  # e.g. Package_DIP:DIP-28_W7.62mm
+
+        return cls(
+            name=symbol.name,
+            pinout=pinout,
+            datasheet=datasheet.value if datasheet else None,
+            description=description.value if description else None,
+            footprint=footprint.value if footprint else None,
+        )
--- kicad_parser/test_kicad_sym.py
+++ kicad_parser/test_kicad_sym.py
@@ -1,11 +1,17 @@
 import pathlib
+from json import dumps
 
 from kicad_sym import KicadLibrary
+from part import Part
+
+
+def get_fixture_file(file: str) -> str:
+    return pathlib.Path(__file__).parent.resolve().__str__() + f'/fixtures/{file}'
+
 
 # https://gitlab.com/kicad/libraries/kicad-symbols/-/raw/master/74xx.kicad_sym?ref_type=heads
 def test_74xx():
-    fixture: str = pathlib.Path(__file__).parent.resolve().__str__() + '/fixtures/74xx.sym'
-    library = KicadLibrary.from_file(fixture)
+    library = KicadLibrary.from_file(get_fixture_file('74xx.sym'))
     # print(library)
 
     assert library.generator == 'kicad_symbol_editor'
@@ -15,4 +21,55 @@ def test_74xx():
     assert [symbol.name for symbol in library.symbols] == ['74469', '7454']
 
     # symbol = library.symbols[0]
-    # print(symbol.pins)
+
+    # etype: tri_state, input, power_in, output
+    # print('Pins', {pin.number:[pin.name, pin.etype] for pin in symbol.pins})
+    # print('Properties', {prop.name.lower(): prop.value for prop in symbol.properties})
+
+
+def test_parse_symbol():
+    library = KicadLibrary.from_file(get_fixture_file('74xx.sym'))
+    symbol = library.symbols[0]
+
+    part = Part.from_kicad_symbol(symbol)
+    # print(part, dumps(part.as_dict(), indent=True))
+
+    assert part.name == '74469'
+    assert part.footprint == ''
+    assert part.description == '8-bit synchronous up/down counter, parallel load and hold capability (obsolete)'
+    assert part.datasheet == 'http://www.ti.com/lit/gpn/sn74469'
+    assert len(part.pinout) == 24
+
+    assert part.pinout['1'].name == 'CLK'
+    assert part.pinout['1'].type == 'input'
+
+    assert part.pinout['12'].name == 'GND'
+    assert part.pinout['12'].type == 'power_in'
+
+    assert part.pinout['24'].name == 'VCC'
+    assert part.pinout['24'].type == 'power_in'
+
+
+def test_parse_symbol_with_inheritance():
+    library = KicadLibrary.from_file(get_fixture_file('attiny.sym'))
+
+    assert len(library.symbols) == 2
+    assert [symbol.name for symbol in library.symbols] == ['ATtiny48-P', 'ATtiny88-P']
+
+    attiny48 = Part.from_kicad_symbol(library.symbols[0])
+    attiny88 = Part.from_kicad_symbol(library.symbols[1])
+
+    # print(str(attiny48), str(attiny88))
+
+    assert attiny48.name == 'ATtiny48-P'
+    assert attiny48.footprint == 'Package_DIP:DIP-28_W7.62mm'
+    assert attiny48.description == '12MHz, 4kB Flash, 256B SRAM, 64B EEPROM, DIP-28'
+    assert attiny88.name == 'ATtiny88-P'
+    assert attiny88.footprint == 'Package_DIP:DIP-28_W7.62mm'
+    assert attiny88.description == '12MHz, 8kB Flash, 512B SRAM, 64B EEPROM, DIP-28'
+
+    # these devices share the same pinout
+    assert attiny48.pinout['1'].name == '~{RESET}/PC6'
+    assert attiny88.pinout['1'].name == '~{RESET}/PC6'
+    assert attiny48.pinout['28'].name == 'PC5'
+    assert attiny88.pinout['28'].name == 'PC5'
