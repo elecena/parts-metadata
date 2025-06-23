e4d014906c0a31a6495949e46e2519cd664bb9ae
macbre <maciej.brencz@gmail.com>
2025-06-23 22:12:25 +0100

    kicad_parser: reformat the code to make black happy

--- kicad_parser/part.py
+++ kicad_parser/part.py
@@ -10,17 +10,18 @@ class Pin:
     """
     Keep a single pin definition, a subset of kicad_sym.Pin class
     """
+
     name: str  # e.g. DO, Q3, GND, CLK, VCC
     type: str  # e.g. tri_state, input, power_in, output, no_connect
     alt_funcs: Optional[list[str]]  # alternative functions, useful for microprocessors
 
     def __repr__(self) -> str:
-        return f'<Pin {self.name} [{self.type}]>'
+        return f"<Pin {self.name} [{self.type}]>"
 
     def as_dict(self) -> dict:
         return {
-            'name': self.name,
-            'type': self.type,
+            "name": self.name,
+            "type": self.type,
         }
 
 
@@ -29,30 +30,28 @@ class Part:
     """
     Keeps the device pinout, a subset of kicad_sym.KicadSymbol class
     """
+
     name: str  # e.g. 74469
-    pinout:  Dict[str, Pin]  # "1": {name: "DO": type: "input"}
+    pinout: Dict[str, Pin]  # "1": {name: "DO": type: "input"}
     footprint: Optional[str]
     datasheet: Optional[str]
     description: Optional[str]
 
     def __str__(self) -> str:
-        return f'<Part {self.name} ({self.description})>'
+        return f"<Part {self.name} ({self.description})>"
 
     def __repr__(self) -> str:
-        pinout = '\n'.join([
-            f'\t{no} {repr(pin)}'
-          for no, pin in self.pinout.items()
-        ])
+        pinout = "\n".join([f"\t{no} {repr(pin)}" for no, pin in self.pinout.items()])
 
-        return f'<Part {self.name}\n\tDesc: {self.description}\n\tPDF: {self.datasheet}\n{pinout}\n>'
+        return f"<Part {self.name}\n\tDesc: {self.description}\n\tPDF: {self.datasheet}\n{pinout}\n>"
 
     def as_dict(self) -> dict:
         return {
-            'name': self.name,
-            'description': self.description,
-            'datasheet': self.datasheet,
-            'footprint': self.footprint,
-            'pinout': {no: pin.as_dict() for no, pin in self.pinout.items()},
+            "name": self.name,
+            "description": self.description,
+            "datasheet": self.datasheet,
+            "footprint": self.footprint,
+            "pinout": {no: pin.as_dict() for no, pin in self.pinout.items()},
         }
 
     def as_yaml(self) -> str:
@@ -61,9 +60,9 @@ class Part:
 
     @classmethod
     def from_dict(cls, data: dict):
-        data['pinout'] = {
-            no: Pin(name=pin['name'], type=pin['type'], alt_funcs=None)
-            for no, pin in data.get('pinout', {}).items()
+        data["pinout"] = {
+            no: Pin(name=pin["name"], type=pin["type"], alt_funcs=None)
+            for no, pin in data.get("pinout", {}).items()
         }
 
         return cls(**data)
@@ -79,7 +78,7 @@ class Part:
         # (symbol "INA281A2"
         #   (extends "INA281A1")
         # (symbol "INA281A1"
-		#   (extends "AD8211")
+        #   (extends "AD8211")
         parent = symbol.get_root_symbol()
 
         pinout = {
@@ -91,15 +90,15 @@ class Part:
             # pins should be sorted by their numbers
             for pin in sorted(
                 parent.pins,
-                key=lambda p: int(p.number) if str(p.number).isnumeric() else p.number
+                key=lambda p: int(p.number) if str(p.number).isnumeric() else p.number,
             )
         }
 
-        assert len(pinout.keys()) > 0, f'Pinout of {symbol.name} is empty'
+        assert len(pinout.keys()) > 0, f"Pinout of {symbol.name} is empty"
 
-        datasheet = symbol.get_property('Datasheet')
-        description = symbol.get_property('Description')
-        footprint = symbol.get_property('Footprint')  # e.g. Package_DIP:DIP-28_W7.62mm
+        datasheet = symbol.get_property("Datasheet")
+        description = symbol.get_property("Description")
+        footprint = symbol.get_property("Footprint")  # e.g. Package_DIP:DIP-28_W7.62mm
 
         return cls(
             name=symbol.name,
